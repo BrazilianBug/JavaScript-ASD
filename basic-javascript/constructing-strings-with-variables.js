e9b1f0a0b76603ec82880d2cd2ee61e7ac5cbac7
BrazilianBug <leandro.smacedo@Live.com>
2023-12-28 17:13:42 +0000

    feat: FreeCodeCamp - 33% Progress

---
+++ basic-javascript/constructing-strings-with-variables.js
@@ -0,0 +1,3 @@
+// Altere apenas o código abaixo desta linha
+const myName = "Leandro";
+const myStr = "Hello, our name is" + myName + ", how are you?";
---
+++ basic-javascript/understand-string-immutability.js
@@ -0,0 +1,7 @@
+// Configuração
+let myStr = "Jello World";
+
+// Altere apenas o código abaixo desta linha
+myStr = "Hello World"; // Altere esta linha
+// Altere apenas o código acima desta linha
+console.log(myStr)
