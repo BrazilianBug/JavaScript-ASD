f3cc92f853e353a81ce55cb193bccc9d7146b5fd
BrazilianBug <leandro.smacedo@Live.com>
2023-12-28 23:00:14 +0000

    feat: ifs

---
+++ basic-javascript/use-conditional-logic-with-if-statements.js
@@ -0,0 +1,11 @@
+function trueOrFalse(wasThatTrue) {
+  // Altere apenas o código abaixo desta linha
+  if (wasThatTrue == true) {
+    return "Yes, that was true"
+  }
+  return "No, that was false"
+
+
+  // Altere apenas o código acima desta linha
+
+}
